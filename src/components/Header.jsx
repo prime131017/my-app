ff0d27f56f620f549b1fe5738f43f58d40e96379
Anatolii <anatolii.hutsul@ukr.net>
2023-05-07 13:26:57 +0200

    Breaking down a project into components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://logos-world.net/wp-content/uploads/2020/04/Adidas-Logo-1971-present.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://th.bing.com/th/id/OIP.avb9nDfw3kq7NOoP0grM4wHaEK?w=322&h=181&c=7&r=0&o=5&dpr=1.4&pid=1.7" />
+            </div>
+            <div>
+                ava+descriptions
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                News posts
+            </div>
+            <div>
+                post1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
