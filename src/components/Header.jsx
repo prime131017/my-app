73ec96abb5b6273903f4a8f6ab2effe9aa80bbd4
Anatolii <anatolii.hutsul@ukr.net>
2023-05-07 20:18:33 +0200

    Added style  modules.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://logos-world.net/wp-content/uploads/2020/04/Adidas-Logo-1971-present.png' />
         </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://th.bing.com/th/id/OIP.avb9nDfw3kq7NOoP0grM4wHaEK?w=322&h=181&c=7&r=0&o=5&dpr=1.4&pid=1.7" />
             </div>
