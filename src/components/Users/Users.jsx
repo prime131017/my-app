7cb3d029e5a01d82604a4306775ead29d4662473
Anatolii <anatolii.hutsul@ukr.net>
2023-08-20 20:47:22 +0200

    Make pagination in Users page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,27 +5,49 @@ import userPhoto from "../../assets/image/samuray.jpeg";
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
+    componentDidMount() {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount)
+
+        });
+    }
+
+    onPageChange =(pageNumber)=>{
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        .then(response => {
             this.props.setUsers(response.data.items)
         });
     }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i < pagesCount; i++) {
+            pages.push(i);
+        }
+
         return <div>
+            <div className={styles.wrapperPage}>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage===p && styles.selectedPage} onClick={(e)=>{this.onPageChange(p)}}>{p}</span>
+                })}
+            </div>
             {
                 this.props.users.map(u => <div key={u.id} className={styles.wrapper}>
                     <span>
                         <div>
                             <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                         </div>
-                        <dir>
+                        <div>
                             {u.followed
                                 ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
                                 : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                        </dir>
+                        </div>
                     </span>
                     <span>
                         <span>
